de9d27c4e3f08598d1054bd5d0d7e0b2fde56a48
StaragileDevops <116961807+StaragileDevops@users.noreply.github.com>
2023-03-04 23:16:12 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -108,5 +108,5 @@ resource "aws_instance" "terraform" {
 
 provisioner "local-exec" {command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u {var.user} -i '${self.ipv4_address},' --private-key ${var.ssh_private_key} playbook.yml"}
 }
-}
+
 
