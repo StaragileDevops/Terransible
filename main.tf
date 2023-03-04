7ce396120f6d7953b9f4f639aee30beeed065d0f
StaragileDevops <116961807+StaragileDevops@users.noreply.github.com>
2023-03-05 00:55:58 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,53 @@
+//Security group creation and whitelisting the ip
+resource "aws_security_group" "allow_tls" {
+  name = "terraform-sg"
+
+  ingress {
+    description = "Allow port 22 - inbound"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow port 8080 - inbound"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.11.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-northeast-1"
+}resource "aws_instance" "myec2" {
+  ami                    = "ami-0b828c1c5ac3f13ee"
+  instance_type          = "t2.micro"
+  availability_zone = "ap-northeast-1a"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name = "Jen"
+
+  tags = {
+    name = "testec2"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.myec2.public_ip} >> /etc/ansible/hosts"
+  }
+}
