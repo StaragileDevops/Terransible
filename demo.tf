47083307a8f70856b1e6a1ad3e09298869fba608
StaragileDevops <116961807+StaragileDevops@users.noreply.github.com>
2023-03-04 14:12:59 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -9,24 +9,18 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIA5QYQHL5JCIEGPHPG"
-  secret_key = "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"
+  region = "ap-northeast-1"
+  
 }
 
 #Define Infrastructure
-resource "aws_instance" "Terraform_demo_instance"{
-	ami = "ami-0557a15b87f6559cf"
-	instance_type = "t2.nano"
+resource "aws_instance" "terraform"{
+	ami = "ami-0b828c1c5ac3f13ee"
+	instance_type = "t2.micro"
 	tags = {
 	Name = "Terraform Instance 1"
 }
+provisioner "local-exec" {
+    command = "echo ${aws_instance.terraform.public_ip} > /etc/ansible/hosts"
 }
-#Define another instance
-resource "aws_instance" "Terraform_another"{
-	ami = aws_instance.Terraform_demo_instance.ami
-	instance_type = "t2.nano"
-	tags = {
-	Name = "Terrafom another"
-}
-}
+
